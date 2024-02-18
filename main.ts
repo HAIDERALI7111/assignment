dca0d6a8e2ee4eb4e2457aaf64bb682be365f368
HAIDERALI7111 <deejay4272@gmail.com>
2024-02-18 14:14:26 -0800

    main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,2 @@
 console.log("thank you miss hina naseer for your guidance to being a smart programmers!");
-console.log("without your help we did not find a valueable matterials for our study");
+console.log("without your help we did not find a valuable materials for our study");

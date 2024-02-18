ff4d443a1628aceba8588011d116f29b02f625e1
HAIDERALI7111 <deejay4272@gmail.com>
2024-02-18 13:54:21 -0800

    my first class commit

---
+++ main.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("thank you miss hina naseer for your guidance to being a smart programmers!");
+console.log("without your help we did not find a valueable matterials for our study");
